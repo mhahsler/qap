61785de2a5c1be583a639531923f2a06841f4342
mhahsler <michael@hahsler.net>
2016-03-27 21:33:04 -0500

    added testthat.

--- tests/testthat/test-read_qaplib.R
+++ tests/testthat/test-read_qaplib.R
@@ -1,9 +1,10 @@
-library(qap)
+library("testthat")
+library("qap")
 
 # can we read all the QAPLIP files?
 problems <- dir(system.file("qaplib", package="qap"), pattern = "*.dat")
 
 for(p in problems) {
   cat("Reading problem", p ,"\n")
-  read_qaplib(system.file("qaplib", p, package="qap"))
+  expect_silent(read_qaplib(system.file("qaplib", p, package="qap")))
 }
